5f101560d568dcb9da8c5aa1340de68b3aa773e6
Parag Gupta <parag.gupta@myntra.com>
2024-09-01 17:57:24 +0530

    Service Created

--- main/model.go
+++ main/model.go
@@ -1,1 +1,15 @@
 package main
+
+import "github.com/graph-gophers/graphql-go"
+
+type User struct {
+	Id         graphql.ID   `json:"id"`
+	Name       string       `json:"name"`
+	Followers  []graphql.ID `json:"followers"`
+	Followings []graphql.ID `json:"followings"`
+}
+
+type UserResponse struct {
+	Id   graphql.ID `json:"id"`
+	Name string     `json:"name"`
+}
